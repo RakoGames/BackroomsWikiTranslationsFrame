ed8cd89ab8a5cd999e80f5db077f0f8d73b243e2
RakoGames <67343902+RakoGames@users.noreply.github.com>
2024-12-02 20:18:55 +0000

    hope this works I guess

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.get('/', async (req, res) => {
 
     for (const lang in branches) {
         if (branches[lang]) {
-            result += template[wmid < embedWidth ? (wbig < embedWidth ? 2 : 1) : 0].replaceAll("HOST", results[lang].host).replaceAll("URL", url).replaceAll("FLAG", results[lang].flag).replaceAll("ALT", results[lang].alt).replaceAll("NAME", results[lang].name[wbig < embedWidth ? 1 : 0]);
+            result += template[wmid < embedWidth ? (wbig < embedWidth ? 2 : 1) : 0].replaceAll("HOST", results[lang].host).replaceAll("URL", url).replaceAll("FLAG", results[lang].flag).replaceAll("ALT", results[lang].alt).replaceAll("NAME", results[lang].name[(wbig < embedWidth ? 1 : 0)]);
         }
     }
 
