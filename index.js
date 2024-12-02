bd1d6bf96220a74728a45035cbc741076eeb7a37
RakoGames <67343902+RakoGames@users.noreply.github.com>
2024-12-02 20:12:55 +0000

    made the languages modular for easier addition of more languages

--- index.js
+++ index.js
@@ -6,105 +6,98 @@ const app = express();
 const port = process.env.PORT || 4000;
 const embedWidth = 350;
 
+let template = ['<a target="_top" href="HOST/URL"><img src="FLAG" class="image" alt="ALT"></a>',
+        '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="FLAG" class="image" alt="ALT"></a><a class="expanded" target="_top" href="HOST/URL">NAME</a></div>',
+        '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="FLAG" class="image" alt="ALT"></a><a class="expanded" target="_top" href="HOST/URL">NAME</a></div>'];
+
 let results = {
     AR: {
         host: 'http://ar-backrooms-wiki.wikidot.com',
         title: 'الغرف الخلفية',
         width: 72.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-saudi-arabia2x.png" class="image" alt="flag-saudi-arabia2x.png"></a>',
-            '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-saudi-arabia2x.png" class="image" alt="flag-saudi-arabia2x.png"></a><a class="expanded" target="_top" href="HOST/URL">AR</a></div>',
-            '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-saudi-arabia2x.png" class="image" alt="flag-saudi-arabia2x.png"></a><a class="expanded" target="_top" href="HOST/URL">Arabic</a></div>'],
-        miss: ''
+        flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-saudi-arabia2x.png',
+        alt: 'saudi arabia',
+        name: ['AR', 'Arabic']
     },
     CN: {
         host: 'http://backrooms-wiki-cn.wikidot.com',
         title: 'The Backrooms中文维基',
         width: 79.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/china" class="image" alt="china"></a>',
-            '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/china" class="image" alt="china"></a><a class="expanded" target="_top" href="HOST/URL">CN</a></div>',
-            '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/china" class="image" alt="china"></a><a class="expanded" target="_top" href="HOST/URL">Chinese</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/china',
+        alt: 'china',
+        name: ['CN', 'Chinese']
     },
     DE: {
         host: 'http://de-backrooms-wiki.wikidot.com',
         title: 'Das Backrooms Wiki auf Deutsch',
         width: 72.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png" class="image" alt="flag-germany2x.png"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png" class="image" alt="flag-germany2x.png"></a><a class="expanded" target="_top" href="HOST/URL">DE</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png" class="image" alt="flag-germany2x.png"></a><a class="expanded" target="_top" href="HOST/URL">German</a></div>'],
-        miss: ''
+        flag: 'https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-germany2x.png',
+        alt: 'germany',
+        name: ['DE', 'German']
     },
     EN: {
         host: 'https://backrooms-wiki.wikidot.com',
         title: 'The Backrooms',
         width: 79.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/uk" class="image" alt="uk"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/uk" class="image" alt="uk"></a><a class="expanded" target="_top" href="HOST/URL">EN</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/uk" class="image" alt="uk"></a><a class="expanded" target="_top" href="HOST/URL">English</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/uk',
+        alt: 'uk',
+        name: ['EN', 'English']
     },
     ES: {
         host: 'http://es-backrooms-wiki.wikidot.com',
         title: 'Wiki Backrooms Español',
         width: 79.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/spain" class="image" alt="spain"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/spain" class="image" alt="spain"></a><a class="expanded" target="_top" href="HOST/URL">ES</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/spain" class="image" alt="spain"></a><a class="expanded" target="_top" href="HOST/URL">Spanish</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/spain',
+        alt: 'spain',
+        name: ['ES', 'Spanish']
     },
     FR: {
         host: 'http://fr-backrooms-wiki.wikidot.com',
         title: 'Backrooms Wiki',
         width: 72.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/france" class="image" alt="france"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/france" class="image" alt="france"></a><a class="expanded" target="_top" href="HOST/URL">FR</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/france" class="image" alt="france"></a><a class="expanded" target="_top" href="HOST/URL">French</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/france',
+        alt: 'france',
+        name: ['FR', 'French']
     },
     ID: {
         host: 'http://id-backrooms-wiki.wikidot.com',
         title: 'Backrooms Indonesia',
         width: 100.3,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/indonesia" class="image" alt="indonesia"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/indonesia" class="image" alt="indonesia"></a><a class="expanded" target="_top" href="HOST/URL">ID</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/indonesia" class="image" alt="indonesia"></a><a class="expanded" target="_top" href="HOST/URL">Indonesian</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/indonesia',
+        alt: 'indonesia',
+        name: ['ID', 'Indonesian']
     },
     IT: {
         host: 'http://it-backrooms-wiki.wikidot.com',
         title: 'Le Backrooms',
         width: 79.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/italy" class="image" alt="italy"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/italy" class="image" alt="italy"></a><a class="expanded" target="_top" href="HOST/URL">IT</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/italy" class="image" alt="italy"></a><a class="expanded" target="_top" href="HOST/URL">Italian</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/italy',
+        alt: 'italy',
+        name: ['IT', 'Italian']
     },
     PL: {
         host: 'http://pl-backrooms-wiki.wikidot.com',
         title: 'Backrooms Polska',
         width: 72.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/poland" class="image" alt="poland"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/poland" class="image" alt="poland"></a><a class="expanded" target="_top" href="HOST/URL">PL</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/poland" class="image" alt="poland"></a><a class="expanded" target="_top" href="HOST/URL">Polish</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/poland',
+        alt: 'poland',
+        name: ['PL', 'Polish']
     },
     PTBR: {
         host: 'http://pt-br-backrooms-wiki.wikidot.com',
         title: 'As Backrooms',
         width: 100.3,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/portugal" class="image" alt="portugal"></a>',
-               '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/portugal" class="image" alt="portugal"></a><a class="expanded" target="_top" href="HOST/URL">PT-BR</a></div>',
-               '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/portugal" class="image" alt="portugal"></a><a class="expanded" target="_top" href="HOST/URL">Portuguese</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick/portugal',
+        alt: 'portugal',
+        name: ['PT-BR', 'Portuguese']
     },
     RU: {
         host: 'http://ru-backrooms-wiki.wikidot.com',
         title: 'База данных Backrooms',
         width: 79.2,
-        href: ['<a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/russia" class="image" alt="russia"></a>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/russia" class="image" alt="russia"></a><a class="expanded" target="_top" href="HOST/URL">RU</a></div>',
-              '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/russia" class="image" alt="russia"></a><a class="expanded" target="_top" href="HOST/URL">Russian</a></div>'],
-        miss: ''
+        flag: 'http://backrooms-sandbox-2.wikidot.com/local--files/zenzick-3/russia',
+        alt: 'russia',
+        name: ['RU', 'Russian']
     },
     TH: {
         host: 'http://th-backrooms-wiki.wikidot.com',
@@ -158,9 +151,7 @@ app.get('/', async (req, res) => {
 
     for (const lang in branches) {
         if (branches[lang]) {
-            result += results[lang].href[wmid < embedWidth ? (wbig < embedWidth ? 2 : 1) : 0];
-        } else {
-            result += results[lang].miss;
+            result += tempalte[wmid < embedWidth ? (wbig < embedWidth ? 2 : 1) : 0].replaceAll("HOST", results[lang].host).replaceAll("URL", url).replaceAll("FLAG", results[lang].flag).replaceAll("ALT", results[lang].alt).replaceAll("NAME", results[lang].name[wbig < embedWidth ? 1 : 0]);
         }
     }
 
