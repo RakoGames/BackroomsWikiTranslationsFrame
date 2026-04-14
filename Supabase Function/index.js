3674898ebb7d78ac3ca1c3bc53007dd241838309
Rako Games <67343902+RakoGames@users.noreply.github.com>
2026-04-14 23:00:49 +0300

    Update index.js

--- Supabase Function/index.js
+++ Supabase Function/index.js
@@ -5,30 +5,30 @@ import { corsHeaders } from "jsr:@supabase/supabase-js/cors";
 import branches from "./branches.json" with { type: "json" };
 import htmlCode from "./website.json" with { type: "json" };
 
+const cacheDuration = 15 * 60; // seconds
+
 Deno.serve(async (req) => {
   try {
     // CORS Check
     if (req.method === "OPTIONS") {
       return new Response("ok", { headers: {...corsHeaders, 'Access-Control-Allow-Headers': 'url, branch, theme, site'}})
     }
-    /*
+    
     // Initialize Supabase Database Client
     const supabase = createClient(
       Deno.env.get("SUPABASE_URL") ?? "",
-      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
       { global: { headers: { Authorization: req.headers.get("Authorization")! } } }
     );
-
-    // TODO: Change the table_name to your table
-    const { data, error } = await supabase.from("table_name").select("*")
-
-    if (error) {
-      throw error
-    }
-    */
+    
 
     // Flag Template
-    let template = '<div style="display: flex;"><a target="_top" href="HOST/URL"><img src="FLAG" class="image" alt="ALT"></a><a class="expanded" target="_top" href="HOST/URL">NAME</a></div>';
+    // HOST is the branch url
+    // URL is the page UNIX url
+    // NAME is the branch's language name
+    // FLAG is the branch's flag url
+    // ALT is the flag's alt text
+    let template = '<a style="padding: 0 5%;" target="_top" href="HOST/URL"><img src="FLAG" class="image" alt="ALT"></a>';
 
     // Get the request parameters
     const query = req.headers;
@@ -37,25 +37,35 @@ Deno.serve(async (req) => {
     const theme = query.get("theme") !== "{$theme}" ? query.get("theme") : "";
     const siteid = query.get("site") ?? 'backrooms-wiki';
 
-    // To be populated with found Branches
-    let matches = {};
-    let result = ""
 
-    // Go through rach branch
+    const SQLData = (await supabase.from("Branches").select("*").eq('url', url)).data;
+    let result = "";
+    let isCached = SQLData.length > 0 && Date.now() - SQLData[0].date < cacheDuration * 1000;
+
+    if (SQLData.length == 0) { SQLData[0] = { 'url': url }; }
+
+    // Go through each branch
     for (const lang in branches) {
-        // Skip the current branch
-        if (branch == lang.toLowerCase()) { continue; }
+      // Skip the current branch
+      if (branch == lang.toLowerCase()) { SQLData[0][lang] = true; continue; }
 
-        // Check if the page exists in the branch
-        if(await CheckSite(branches[lang].host, url, branches[lang].title)) {
-            // Generate the resulting page
-            result += template.replaceAll("HOST", branches[lang].host).replaceAll("URL", url).replaceAll("FLAG", branches[lang].flag).replaceAll("ALT", branches[lang].alt).replaceAll("NAME", branches[lang].name[1]);
-        };
+      // Check if the page exists in the branch
+      if(isCached ? SQLData[0][lang] == true : await CheckSite(branches[lang].host, url, branches[lang].title)) {
+          // Generate the resulting page
+          result += template.replaceAll("HOST", branches[lang].host).replaceAll("URL", url).replaceAll("FLAG", branches[lang].flag).replaceAll("ALT", branches[lang].alt).replaceAll("NAME", branches[lang].name[1]);
+          SQLData[0][lang] = true;
+      } else {
+        SQLData[0][lang] = false;
+      }
+
+      console.log(`URL: ${url}, isCached: ${isCached}, SQLData[0][${lang}] = ${SQLData[0][lang]}`);
     }
+
+    SQLData[0].date = Date.now();
+
+    if (!isCached) await supabase.from("Branches").upsert(SQLData[0], { onConflict: 'url' });
     
     let page = htmlCode["htmlCode"].replaceAll("THING GOES HERE", result).replaceAll("THEME", theme).replaceAll("SITEID", siteid);
-
-    console.log(page);
 
     // Return the page
     return new Response(page, {
@@ -71,6 +81,7 @@ Deno.serve(async (req) => {
 })
 
 let CheckSite = function(host, url, title) {
+    console.log(`HI, LangURL: ${host}/${url}`);
     return new Promise(async (resolve, reject) => {
         try {
             // Get the page's source
